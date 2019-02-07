267064a36d3a30d01fafd54ad8f298b39394bc59
kosalaat <kosala@kosala.net>
2019-02-07 23:41:54 +1300

    fixed the exceptions import, and urlencode formatting for filtervalues

--- Actifio/Actifio.py
+++ Actifio/Actifio.py
@@ -7,11 +7,13 @@ import urllib3
 import sys
 import json
 from functools import wraps
+from ActHosts import ActHosts
 
+# import custom exceptions 
 if sys.version [:3] == "2.7":
-  import actexceptions
+  from actexceptions import *
 elif sys.version[0] == "3":
-  import Actifio.actexceptions
+  from Actifio.actexceptions import *
 
 # Import urlencode for the correct version
 if sys.version [:3] == "2.7":
@@ -36,34 +38,28 @@ class ActEnforce():
     def decorated(cls, *args, **kwargs):
       if Actifio._sessionid[cls.appliance] == '':
         try:
-          print("session is empty, trying to create the session")
           Actifio._create_session(cls)
         except:
           raise
-      print("Session seems to be good")
       try:
         result = act_func(cls,*args, **kwargs)
       except:
-        print(act_func.__name__ + "failed")
         try:
           if not Actifio._validate_token(cls):
             try:
               Actifio._create_session(cls)
-              print("created the session")
             except:
-              print(" second create_session excemption")
               raise
             try:
               result = act_func(cls,*args, **kwargs)
             except:
-              print(act_func.__name__ + "failed")
+              # print(act_func.__name__ + "failed")
               raise
             else:
               return result
           else:
             raise
         except:
-          print("validate token failed..")
           raise
       else:
         return result
@@ -74,12 +70,13 @@ class Actifio:
   #
   _sessionid = {}
   
-  def __init__ (self, appliance, username, password, vendorkey, cert_validation=False):
+  def __init__ (self, appliance, username, password, vendorkey="python-wrapper", cert_validation=False):
     # functools need a __name__ for the wraps
     self.__name__ = "Actifio"
     # compose the URI for the API
     self._apiBase = "/actifio/api"
     self.appliance = appliance
+    self.username = username
     Actifio._sessionid.update( { appliance: '' } )
     # compose the login params
     self._loginParams = "/login?name=" + username 
@@ -95,6 +92,12 @@ class Actifio:
     cert_str = 'CERT_REQUIRED' if cert_validation else 'CERT_NONE'
 
     self._httppool = urllib3.HTTPSConnectionPool (host=appliance, port=443, cert_reqs=cert_str)
+
+  def __str__(self):
+    if Actifio._sessionid[self.appliance] == "":
+      return "Connection not verified; Appliance: " + str(self.appliance) + "; Username: " + str(self.username)
+    else:
+      return "Connection verified (session id): " + str(Actifio._sessionid[self.appliance]) + "; Appliance: " + str(self.appliance) + "; Username: " + str(self.username)
 
   @staticmethod
   def _validate_token (self):
@@ -146,7 +149,7 @@ class Actifio:
 
 
   @ActEnforce.needs_token
-  def run_uds_command(self, cmdType, cmdUDS, cmdArgs):
+  def run_uds_command(self, cmdType, cmdUDS, cmdArgs={}):
     """
     Wrapper function to convert CLI commands to the rest API.
       cmdType: info / task 
@@ -175,22 +178,21 @@ class Actifio:
     for key in cmdArgs:
       if type(cmdArgs[key]) == dict:
         # Actifio API expects this to be urlencoded
-        _URI += key + '=' + urlencode_str(urlencode(cmdArgs[key])) + '&' 
+        _URI += key + '=' + urlencode_str('&'.join([ filter_key + '=' + cmdArgs[key][filter_key] for filter_key in cmdArgs[key]])) + '&'
       elif cmdArgs[key] == None:
-        _URI += key + '&'
+        _URI += urlencode_str(key) + '&'
       else:
-        _URI += key + '=' + str(cmdArgs[key]) + '&'
+        _URI += urlencode_str(key) + '=' + urlencode_str(str(cmdArgs[key])) + '&'
         
     _URI += 'sessionid=' + Actifio._sessionid[self.appliance]
-  
     try:
       udsout = self._httppool.request (
         'GET' if cmdType == 'info' else 'POST',
         _URI
-      )
+      )  
     except Exception as e:
-      print(type(e))
-      raise ActExceptionConnect
+      print (e)
+      raise ActConnectError("Failed to connect the appliance")
     else:
       response = json.loads(udsout.data)
       if udsout.status != 200:
@@ -221,14 +223,13 @@ class Actifio:
     for key in cmdArgs:
       if type(cmdArgs[key]) == dict:
         # Actifio API expects this to be urlencoded
-        _URI += key + '=' + urlencode_str(urlencode(cmdArgs[key])) + '&' 
+        _URI += key + '=' + urlencode_str('&'.join([ filter_key + '=' + cmdArgs[key][filter_key] for filter_key in cmdArgs[key]])) + '&'
       elif cmdArgs[key] == None:
-        _URI += key + '&'      
+        _URI += urlencode_str(key) + '&'
       else:
-        _URI += key + '=' + str(cmdArgs[key]) + '&'
+        _URI += urlencode_str(key) + '=' + urlencode_str(str(cmdArgs[key])) + '&'
         
     _URI += 'sessionid=' + Actifio._sessionid[self.appliance]
-
     try:
       sargout = self._httppool.request (
         'GET',
@@ -246,16 +247,64 @@ class Actifio:
         return self._lastout
 
   
-  def create_or_replace_iscsi_host(self, name, ipaddress, host_type='generic', dsik_pref='BLOCK', detect_as_vm=False):
+  def define_iscsi_host(self, hostname, ipaddress, host_type='geenric', dsik_pref='BLOCK', detect_as_vm=False):
     #check whether the host entry exists
     try:
-      lshost = self.run_uds_command('info', 'lshost',{ 'filtervalue': { 'hostname': name}})
+      lshost = self.run_uds_command('info', 'lshost',{ 'filtervalue': { 'hostname': hostname }})
     except:
       raise
     else:
-      if 1 > len(lshost['result']) > 0:
+      if len(lshost['result']) > 0:
         #found the host:
-        host_id = lshost['result'][0]['id']
+        host = lshost['result']
       else:
         self.run_uds_command('task','chhost',{ 'hostname': name})
 
+  def clone_database(self, source_hostname, source_appname, target_hostname, **kwargs):
+    '''
+    This method creates a virtual clone of Oracle or SQL server database.
+      source_hostname: Hostname of the source host where the database was captured from
+      source_appname: source application name, or the database name
+      target_hostname: target host where the virtual clone need to be created on
+
+      Oracle Related Parameters:
+        oracle_home (required): ORACLE_HOME
+        oracle_db_name (required): SID of the target clone
+        oracle_tns_admin (optional): TNS admin path
+        oracle_db_mem (optional): Total Memory Target for the database
+        oracle_sga_pct (optional): Memory Percentage to allocate for SGA
+        oracle_redo_size (optional): Redo Log size in MB, defaults to 500
+        oracle_shared_pool (optional): Oracle Shared Pool size
+        oracle_db_cache_size (optional): Oracle DB Cache size
+        oracle_recover_dest_size (optional): Oracle Parameter db_recover_dest_size. Defaults to 5000
+        oracle_diagnostic_dest (optional): Oracle Diagnostic Destination
+        oracle_nprocs (optional): Num of Max processes
+        oracle_open_cursors (optional): Number of open_cursors. defaults to 1000 
+        oracle_char_set (optional): Characterset. Defaults to 'AL32UTF8'
+        oracle_tns_ip (optional): TNS IP Address
+        oracle_tns_port (optional): TNS Port
+        oracle_tns_domain (optional): TNS Domain
+        oracle_no_nid (optional): Do not change the DBID of the new clone. Will maintain same DBID as the source. Defaults to FALSE
+        oracle_no_tns_update (optional): Do not update TNS records. Defaults to FALSE
+        oracle_restore_recov (optional): Recover the oracle database. Defaults to TRUE
+        oracle_no_rac (optional): Treat as Oracle RAC. Defaults to TRUE
+
+      SQLServer Related
+        sql_instance_name (required): Target SQL Server instance name
+        sql_recover_userlogins (optional): Recover user logins of the database. Defaults to FALSE
+        sql_username (optional): Username for database provisioning
+        sql_password (optional): Password for the specified user 
+        
+      SQLServer DB Application
+        sql_db_name (reuired): Database name at the target instance. (Only required if the source application is database or single database mount from instance.) 
+
+      SQLServer Instance 
+        sql_source_dbnames (required): Source database names if the source application is SQL instance. Use ',' as delimiter for multiple databases. (Only required if the source application is SQL server instance.) 
+        sql_cg_name (required): Consistency group name. (Only required if the source application is SQL Server instance and mount multiple databases at a time.)
+        sql_dbname_prefix (optional): Prefix of database name for multiple database mount
+        sql_dbname_suffix (optional): Suffix of database name for multiple database mount
+
+    '''
+
+    application = self.run_uds_command("info","lsapplication", {"filtervalue": { "appname": source_appname, "hostname": source_hostname, "apptype!": "VMBackup" }})
+    pass
--- Actifio/actexceptions.py
+++ Actifio/actexceptions.py
@@ -4,12 +4,12 @@ if sys.version [:3] == "2.7":
 
 class ActLoginError(Exception):
   def __init__(self, msg):
-    Exception.__init__(self, msg)
+    Exception.__init__(self, "Actifio Login Exception: " + str(msg))
 
 class ActConnectError (Exception):
   def __init__(self, msg):
-    Exception.__init__(self, msg)
+    Exception.__init__(self, "Actifio Connection Error: " + str(msg))
 
 class ActAPIError (Exception):
   def __init__(self, msg):
-    Exception.__init__(self, msg)
+    Exception.__init__(self, "Actifio API Error: " + str(msg))
