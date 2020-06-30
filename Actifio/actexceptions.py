dcb0d8eae42f2ca01d085362394dc926574febfe
Maxence <max@23.tf>
2020-06-30 02:37:14 +0000

    Fix and clean up exception definitions

--- Actifio/actexceptions.py
+++ Actifio/actexceptions.py
@@ -1,43 +1,39 @@
 import sys
+
 if sys.version [:3] == "2.7":
   import exceptions
 
-class ActException (Exception):
+class ActException(Exception):
   def __init__(self, message):
     super(ActException, self).__init__(message)
     self.message = message
-    self.error_code = ""
-
-# this is still supported in 3.7
-  def __str__(self):
-    if self.error_code != "":
-      return "[" + self.error_code + "] " + self.message
-    else:
-      return self.message
 
 class ActLoginError(ActException):
   def __init__(self, msg):
-    Exception.__init__(self, "Actifio Login Exception: " + str(msg))
+    super(ActLoginError, self).__init__('Actifio Login Exception: %s' %  msg)
     self.msg = msg
 
-class ActConnectError (ActException):
+class ActConnectError(ActException):
   def __init__(self, msg):
-    Exception.__init__(self, "Actifio Connection Error: " + str(msg))
+    super(ActConnectError, self).__init__('Actifio Connection Error: %s' %  msg)
     self.msg = msg
 
-class ActAPIError (ActException):
-  def __init__(self, msg, error_code):
-    Exception.__init__(self, "Actifio API Error: " + str(msg))
+class ActAPIError(ActException):
+  def __init__(self, msg, error_code=''):
+    if error_code != '':
+      super(ActAPIError, self).__init__('[%s] Actifio API Error: %s' %  (error_code, msg))
+    else:
+      super(ActAPIError, self).__init__('Actifio API Error: %s' %  msg)
     self.msg = msg
     self.error_code = error_code
 
-class ActUserError (ActException):
+class ActUserError(ActException):
   def __init__(self, msg):
-    Exception.__init__(self, "Actifio User Error: " + str(msg))
+    super(ActUserError, self).__init__('Actifio User Error: %s' %  msg)
     self.msg = msg
 
-class ActVersionError (ActException):
+class ActVersionError(ActException):
   def __init__(self, object_name, min_version):
-    Exception.__init__(self, "Actifio Version Error: The " + str(object_name) + " is not supported with the current version: min version == " + str(min_version))
+    super(ActVersionError, self).__init__('Actifio Version Error: "%s" is not supported with the current version. Min version == %s' %  (object_name, min_version))
     self.object_name = object_name
     self.min_version = min_version
